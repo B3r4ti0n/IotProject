d8c33edcb779f74f487c26fed271b4f4aeb9878f
angel_rafael <generalpompeyo@hotmail.com>
2023-06-01 14:37:05 +0200

    improvements in match

--- iotinder/src/app/screens/match/Match.js
+++ iotinder/src/app/screens/match/Match.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import mqtt from "precompiled-mqtt";
 import questionsList from "../../assets/questions";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
@@ -9,9 +10,10 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 function Match() {
   const navigate = useNavigate();
-  const [loading, setLoading] = useState("true");
+  const [client, setClient] = useState(null);
   const [activeQuestion, setActiveQuestion] = useState();
   const [idList, setIdList] = useState([]);
+  const url = "mqtt://test.mosquitto.org:8080";
 
   function getNewRandomQuestion() {
     const randomQuestion =
@@ -31,9 +33,40 @@ function Match() {
     }
   }
 
+  const mqttConnect = () => {
+    const client = mqtt.connect(url);
+    setClient(client);
+  };
+
   useEffect(() => {
+    mqttConnect();
     loadQuestion();
   }, []);
+
+  // MQTT client update
+  useEffect(() => {
+    if (client) {
+      client.on("connect", () => {
+        console.log("connected");
+        //client.publish("test", "toto");
+        //setConnectStatus("Connected");
+      });
+
+      client.on("error", (err) => {
+        console.error("Connection error: ", err);
+        client.end();
+      });
+
+      client.on("reconnect", () => {
+        //setConnectStatus("Reconnecting");
+      });
+
+      client.on("message", (topic, message) => {
+        const payload = { topic, message: message.toString() };
+        console.log("payload", payload);
+      });
+    }
+  }, [client]);
 
   function handleAnswer(answer) {
     if (idList.length <= 10) {
@@ -118,3 +151,13 @@ function Match() {
 }
 
 export default Match;
+
+/*
+, {
+  // clientId: "mqttjs_" + Math.random().toString(16).substr(2, 8),
+  connectTimeout: 5000,
+  hostname: "x.xxx.xx.xxx",
+  port: 8000,
+  path: "/mqtt",
+}
+*/
